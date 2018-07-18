513afe8cce1f8eaa32ee46326bd3c301a4894189
jsulam <jeresulam@gmail.com>
2018-07-18 15:45:33 +0300

    adding my implementation of Joint sparse

--- dev/ML_ISTA/Models_MNIST.py
+++ dev/ML_ISTA/Models_MNIST.py
@@ -67,7 +67,138 @@ class ML_ISTA_NET(nn.Module):
         out = F.log_softmax(out,dim = 1)
     
         return gamma, out
+   
+
+
+
+####################################################################################################
+
+
+
+
+##################################################
+
+####        MultiLayer J-ISTA NET (J)        ####
+
+##################################################
+
+class ML_JISTA_NET_J(nn.Module):
+    def __init__(self,m1,m2,m3):
+        super(ML_JISTA_NET_J, self).__init__()
+        
+        # Convolutional Filters
+        self.W1 = nn.Parameter(torch.randn(m1,1,6,6), requires_grad=True)
+        self.strd1 = 2;
+        self.W2 = nn.Parameter(torch.randn(m2,m1,6,6), requires_grad=True)
+        self.strd2 = 2;
+        self.W3 = nn.Parameter(torch.randn(m3,m2,4,4), requires_grad=True)
+        self.strd3 = 1;
+        
+        # Biases / Thresholds
+        self.b1 = nn.Parameter(torch.zeros(1,m1,1,1), requires_grad=True)
+        self.b2 = nn.Parameter(torch.zeros(1,m2,1,1), requires_grad=True)
+        self.b3 = nn.Parameter(torch.zeros(1,m3,1,1), requires_grad=True)
+        
+        # Classifier
+        self.Wclass = nn.Linear(m3, 10)
+        
+        # Initialization
+        self.W1.data = 0.01 * self.W1.data
+        self.W2.data = 0.01 * self.W2.data
+        self.W3.data = 0.01 * self.W3.data
+        
+
     
+    def forward_joint(self, x, b_y,T=0,RHO=1):
+        
+        # Encoding
+        gamma1 = F.relu(F.conv2d(x,self.W1, stride = self.strd1) + self.b1)       # first estimation
+        gamma2 = F.relu(F.conv2d(gamma1,self.W2, stride = self.strd2) + self.b2) 
+        # gamma_3 = F.relu(F.conv2d(gamma2,W3, stride = strd3) + b3) 
+        X = F.conv2d(gamma2,self.W3, stride = self.strd3)
+        gamma3 = torch.zeros(X.shape)
+        if X.device.type=='cuda': gamma3 = gamma3.cuda()
+        for i in range(10):
+            n_ci = float(torch.sum(b_y==i))
+            if n_ci>0:
+                X_i = X[b_y==i,:,:,:]
+                x_i = torch.sqrt(torch.sum(X_i**2,dim=0)) # computing norm of rows
+                a_i = F.relu(x_i + self.b3 ) 
+                X_i = X_i/x_i
+                X_i = X_i * a_i
+                gamma3[b_y==i,:,:,:] = X_i
+     
+        for _ in  range(T):
+            
+            # backward computatoin
+            gamma2_ml = F.conv_transpose2d(gamma3,self.W3, stride=self.strd3)
+            gamma1_ml = F.conv_transpose2d(gamma2_ml,self.W2, stride=self.strd2)
+            
+            gamma1 = (1-RHO) * gamma1 + RHO * gamma1_ml
+            gamma2 = (1-RHO) * gamma2 + RHO * gamma2_ml
+            
+            # forward computation
+            gamma1 = F.relu( (gamma1 - F.conv2d( F.conv_transpose2d(gamma1,self.W1, stride = self.strd1) - x ,self.W1, stride = self.strd1)) + self.b1)
+            gamma2 = F.relu( (gamma2 - F.conv2d( F.conv_transpose2d(gamma2,self.W2, stride = self.strd2) - gamma1, self.W2, stride = self.strd2)) + self.b2) 
+            
+            X = (gamma3 - F.conv2d( F.conv_transpose2d(gamma3,self.W3, stride = self.strd3) - gamma2, self.W3, stride = self.strd3))
+            gamma3 = gamma3*0
+            for i in range(10):
+                n_ci = float(torch.sum(b_y==i))
+                if n_ci>0:
+                    X_i = X[b_y==i,:,:,:]
+                    x_i = torch.sqrt(torch.sum(X_i**2,dim=0)) # computing norm of rows
+                    a_i = F.relu(x_i + self.b3 )
+                    X_i = X_i/x_i
+                    X_i = X_i * a_i
+                    gamma3[b_y==i,:,:,:] = X_i
+        
+        # classifier
+        gamma = gamma3.view(gamma3.shape[0],gamma3.shape[1]*gamma3.shape[2]*gamma3.shape[3])
+        out = self.Wclass(gamma)
+        out = F.log_softmax(out,dim = 1)
+    
+        return gamma, out
+    
+     
+    
+    def forward(self, x,T=0,RHO=1):
+        
+        # Encoding
+        gamma1 = F.relu(F.conv2d(x,self.W1, stride = self.strd1) + self.b1)       # first estimation
+        gamma2 = F.relu(F.conv2d(gamma1,self.W2, stride = self.strd2) + self.b2) 
+        gamma3 = F.relu(F.conv2d(gamma2,self.W3, stride = self.strd3) + self.b3) 
+        
+        for _ in  range(T):
+            
+            # backward computatoin
+            gamma2_ml = F.conv_transpose2d(gamma3,self.W3, stride=self.strd3)
+            gamma1_ml = F.conv_transpose2d(gamma2_ml,self.W2, stride=self.strd2)
+            
+            gamma1 = (1-RHO) * gamma1 + RHO * gamma1_ml
+            gamma2 = (1-RHO) * gamma2 + RHO * gamma2_ml
+            
+            # forward computation
+            gamma1 = F.relu( (gamma1 - F.conv2d( F.conv_transpose2d(gamma1,self.W1, stride = self.strd1) - x ,self.W1, stride = self.strd1)) + self.b1)
+            gamma2 = F.relu( (gamma2 - F.conv2d( F.conv_transpose2d(gamma2,self.W2, stride = self.strd2) - gamma1, self.W2, stride = self.strd2)) + self.b2) 
+            gamma3 = F.relu( (gamma3 - F.conv2d( F.conv_transpose2d(gamma3,self.W3, stride = self.strd3) - gamma2, self.W3, stride = self.strd3)) + self.b3) 
+
+        
+        # classifier
+        gamma = gamma3.view(gamma3.shape[0],gamma3.shape[1]*gamma3.shape[2]*gamma3.shape[3])
+        out = self.Wclass(gamma)
+        out = F.log_softmax(out,dim = 1)
+        
+        
+        return gamma, out
+    
+    
+    
+    
+####################################################################################################    
+    
+    
+
 
 ##################################################
 
